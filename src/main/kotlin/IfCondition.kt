1112972b29cddc22e49983406e27902caf0ea93f
ErtanSidar <essoft3@gmail.com>
2024-12-21 20:25:24 +0300

    if condition

---
+++ src/main/kotlin/IfCondition.kt
@@ -0,0 +1,20 @@
+package org.essoft
+
+fun main() {
+    val x = 5
+
+    if (x > 0) {
+        println("x is positive")
+    } else if (x < 0) {
+        println("x is negative")
+    } else {
+        println("x is zero")
+    }
+
+    if (x % 2 == 0) {
+        println("x is even")
+    } else {
+        println("x is odd")
+    }
+
+}
